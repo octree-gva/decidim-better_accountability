8204aa0e3091d8187e21faed81bb53598cca5453
Renato <renato@octree.ch>
2025-11-17 06:49:21 -0300

    Fix ux title overlap

--- app/helpers/decidim/better_accountability/better_accountability_helper.rb
+++ app/helpers/decidim/better_accountability/better_accountability_helper.rb
@@ -19,11 +19,9 @@ module Decidim
         @better_view_params ||= better_view_query || "grid"
       end
 
-
       def better_view_query
         @better_view_query ||= params.permit(:better_view)[:better_view]
       end
-
 
       def pagination_params
         params.permit(:per_page, :page)
