5d3b661e805eaedaf2b942c5869e09ac1143f16a
Hadrien Froger <hadrien@octree.ch>
2026-01-29 09:28:51 +0000

    release(0.1.3): release layouts fixes

--- lib/decidim/better_accountability/version.rb
+++ lib/decidim/better_accountability/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   module BetterAccountability
     def self.version
-      "0.1.2" # DO NOT UPDATE MANUALLY
+      "0.1.3" # DO NOT UPDATE MANUALLY
     end
 
     def self.decidim_version
